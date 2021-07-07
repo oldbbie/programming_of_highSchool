67896424b0139d0d2085727b9e12405fecf1ca7b
oldbbie <tjd0725@naver.com>
2021-07-08 01:59:18 +0900

    Add p2-030.c

---
+++ p2-030.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	int i,j;
+	
+	for(i = 1; i <= 9; i++)
+	{
+		printf("\n\n%d´Ü\n",i);
+		
+		for(j = 1; j <= 9; j++)
+		{
+			printf("%d * %d = %d	",i,j,i*j);
+		}
+	}
+	
+	return 0;
+}
