c4efc5b7ea7468d984f65902e074d2efca0532ae
oldbbie <tjd0725@naver.com>
2021-08-03 02:54:15 +0900

    Add p3-001.cpp

---
+++ p3-001.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void max_num(int i,int j,int k)
+{
+	int a;
+	a=i;
+	if(a>j)
+		a=j;
+	if(a>k)
+		a=k;
+	printf("세 개의 숫자 중 가장 큰 수는 %d 입니다.",a);
+}
+
+int main()
+{
+	int i,j,k;
+	printf("세 개의 숫자를 입력하시오\n\n");
+	scanf("%d%d%d",&i,&j,&k);
+	printf("입력받은 세 개의 숫자는 %d, %d, %d 입니다.\n\n",i,j,k);
+	max_num(i,j,k);
+	return 0;
+}
+
+
